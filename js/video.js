6ab24d4a82ba261f96d8934f186192c706f3b37a
Daniel-Chatman <68071557+Daniel-Chatman@users.noreply.github.com>
2022-11-29 23:51:11 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -19,7 +19,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 
 	console.log("Play Video");
-
+	video = document.querySelector("#player1");
 	if (video.paused == true){
 		video.play();
 		video.volume = document.querySelector("#slider").value / 100;
@@ -39,7 +39,7 @@ document.querySelector("#play").addEventListener("click", function() {
 
 // Slow Down		-	Slow the current video speed by 10% each time the button is clicked and log the new speed to the console.  
 document.querySelector("#slower").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 	video.playbackRate = video.playbackRate * .9;
 	console.log("The New video speed is: " + video.playbackRate);
 
@@ -50,7 +50,7 @@ document.querySelector("#slower").addEventListener("click", function() {
 // 						Change it by an amount proportional to the slow down.
 // 						If you slow down three times and then speed up three times you should be within 5 digits of 100% again.
 document.querySelector("#faster").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 	video.playbackRate = video.playbackRate * (1/.9);
 	console.log("The New video speed is: " + video.playbackRate);
 
@@ -61,7 +61,7 @@ document.querySelector("#faster").addEventListener("click", function() {
 //						If the video length has been exceeded go back to the start of the video - no farther.
 //						Log the current location of the video.
 document.querySelector("#skip_forward").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 	if(video.currentTime + 2 > video.duration){
 
 		video.currentTime = 0;
@@ -80,7 +80,7 @@ document.querySelector("#skip_forward").addEventListener("click", function() {
 
 // Skip Back
 document.querySelector("#skip_backward").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 	if(video.currentTime - 2 < 0){
 
 		video.currentTime = 0;
@@ -99,7 +99,7 @@ document.querySelector("#skip_backward").addEventListener("click", function() {
 
 // Restart
 	document.querySelector("#restart").addEventListener("click", function() {
-
+		video = document.querySelector("#player1");
 		video.currentTime = 0;
 
 	console.log("The current location of the video: " + video.currentTime);
@@ -108,7 +108,7 @@ document.querySelector("#skip_backward").addEventListener("click", function() {
 
 // Mute				-	Mute/unmute the video and update the text in the button.
 document.querySelector("#mute").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 	if (!video.muted){
 
 		video.muted = true;
@@ -129,7 +129,7 @@ document.querySelector("#mute").addEventListener("click", function() {
 
 // Volume Slider	-	Change the volume based on the slider and update the volume information.
 document.querySelector("#slider").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 console.log("In the video query");
 console.log("Video volume is now " + document.querySelector("#slider").value + "%");
 
@@ -140,7 +140,7 @@ document.querySelector("#volume").innerHTML= (document.querySelector("#slider").
 
 // Styled			-	Utilize the existing oldSchool class on the video element
 document.querySelector("#vintage").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 console.log("Video is now styled");
 video.classList.add("oldSchool");
 	
@@ -148,7 +148,7 @@ video.classList.add("oldSchool");
 
 // Original			-	Remove the oldSchool class from the video.
 document.querySelector("#orig").addEventListener("click", function() {
-
+	video = document.querySelector("#player1");
 console.log("Video is now unstyled");
 video.classList.remove("oldSchool");
 	
